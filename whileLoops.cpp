9f7d0b2255eb01f57c40a781cc96d82d56e4c364
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-28 16:26:54 +0000

    while loops

---
+++ whileLoops.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This Program calculate sum with while loops
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // This Program calculate sum with while loops
+    int loopNumber = 0;
+    int answerNumber = 0;
+
+    std::string userString;
+    int userNumber;
+
+    // input
+    std::cout << "Please enter an positve integer: ";
+    std::cin >> userString;
+    std::cout << "" << std::endl;
+
+    // process
+    try {
+        userNumber = std::stoi(userString);
+        while (loopNumber < userNumber) {
+            loopNumber = loopNumber + 1;
+            answerNumber = answerNumber + loopNumber;
+        }
+        std::cout << "The sum of all positve number from 1 to "
+        << userNumber << " is " << answerNumber << std::endl;
+    } catch (std::invalid_argument) {
+        // output
+        std::cout << userString << " is not a valid input." << std::endl;
+    }
+
+    std::cout << "\nDone." << std::endl;
+}
